a5b39c99fe84fade4044f16a569dabe6f4cb4a3a
PAVAN467 <53818247+PAVAN467@users.noreply.github.com>
2022-05-13 21:10:21 +0530

    Update helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
-
-
+#! /bin/bash
 n=1
 
 while [ $n -le 10 ]
 do
   case $n in
-     [ 1-4 ]) echo "Helloworld $n" ;;
-     [ 4-8 ]) echo "Helloworld $n" ;;
-     * ) echo "Inavlid numbers" ;;
+    [1-4]) echo "Helloworld $n" ;;
+    [5-9]) echo "Helloworld $n" ;;
+    * ) echo "Inavlid numbers" ;;
   esac
   (( n ++ ))
 done
